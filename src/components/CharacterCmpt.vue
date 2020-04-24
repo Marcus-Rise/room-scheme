5ea6a8f2a785a2967d6d3a5132e2d86ac43665d7
Ilya Konstantinov <ilyakonst95@gmail.com>
2020-04-24 20:32:32 +0400

    feat: add character components

---
+++ src/components/CharacterCmpt.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+    component(
+        :is="type"
+        :x="character.x"
+        :y="character.y"
+        :angle="character.angle"
+    )
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+    import {Character} from "@/models/Character";
+    import SimpleCharacter from "@/components/SimpleCharacter.vue";
+
+    @Component
+    export default class CharacterCmpt extends Vue {
+        @Prop() readonly character!: Character;
+
+        get type(): Function {
+            let type: Function;
+
+            switch (this.character.type.name) {
+                case "simple":
+                default:
+                    type = SimpleCharacter;
+                    break;
+            }
+
+            return type;
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    :host {
+        position: absolute;
+    }
+</style>
