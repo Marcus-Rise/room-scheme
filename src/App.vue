a51bd68d874ab958323d146f74d0c7e02a87d260
Ilya Konstantinov <ilyakonst95@gmail.com>
2020-04-24 19:33:59 +0400

    feat(Layout.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
     #app
+        layout
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
     import {Component, Prop} from "vue-property-decorator";
     import {Scheme} from "@/models/Scheme";
+    import {Dictionary} from "@/models/Dictionary";
+    import Layout from "@/components/Layout.vue";
 
-    @Component
+    @Component({
+        components: {Layout}
+    })
     export default class extends Vue {
         @Prop({default: () => new Scheme()}) readonly scheme!: Scheme;
         @Prop({default: () => []}) readonly dictionaries!: readonly Dictionary[];
---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    .card(
+        v-bind="$attrs"
+        v-on="$listeners"
+    )
+        .card-body
+            slot
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Layout extends Vue {
+    }
+</script>
+
+<style lang="scss">
+    @import "~bootstrap/scss/functions";
+    @import "~bootstrap/scss/variables";
+    @import "~bootstrap/scss/mixins";
+    @import "~bootstrap/scss/card";
+</style>
