61fc311b9633f27c3fef01c8ed1a3f840c18ad23
Ilya Konstantinov <ilyakonst95@gmail.com>
2020-04-24 20:07:49 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import data from "./data.json";
+import dictionary from "./dictionary.json";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App, {
+    attrs: {
+      "data-scheme": data,
+      "data-dictionaries": dictionary,
+    }
+  })
 }).$mount('#app')
