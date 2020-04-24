4a5ff591e44390148314c1122f425806fbf27c01
Ilya Konstantinov <ilyakonst95@gmail.com>
2020-04-24 20:53:42 +0400

    perf(App.vue)

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
     import Vue from 'vue';
     import {Component, Prop} from "vue-property-decorator";
     import {ISchemeDto, Scheme} from "@/models/Scheme";
-    import {Dictionary, IDictionaryDto} from "@/models/Dictionary";
     import Layout from "@/components/Layout.vue";
     import CharacterCmpt from "@/components/Character/CharacterCmpt.vue";
 
@@ -23,17 +22,11 @@
     })
     export default class extends Vue {
         @Prop() readonly dataScheme!: ISchemeDto;
-        @Prop() readonly dataDictionaries!: readonly IDictionaryDto[];
 
         scheme: Scheme = new Scheme();
-        dictionaries: Dictionary[] = [];
 
         created() {
             this.scheme = new Scheme(this.dataScheme);
-
-            this.dataDictionaries.forEach((i) => {
-                this.dictionaries.push(new Dictionary(i));
-            });
         }
     }
 </script>
