4eab15eedfcda90b3a9f0301215636cc29abe080
Ilya Konstantinov <ilyakonst95@gmail.com>
2020-04-24 19:08:25 +0400

    feat(App.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+    #app
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+    import Vue from 'vue';
+    import {Component, Prop} from "vue-property-decorator";
+    import {Scheme} from "@/models/Scheme";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+    @Component
+    export default class extends Vue {
+        @Prop({default: new Scheme()}) readonly scheme!: Scheme;
+
+    }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
