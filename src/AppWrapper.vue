0c5968f37ce4176a1e9019c97db0ea1c8b1121ac
Ilya Konstantinov <ilykonst95@gmail.com>
2020-05-12 19:29:33 +0400

    feat(AppWrapper.vue): test chair choosing

--- src/AppWrapper.vue
+++ src/AppWrapper.vue
@@ -1,13 +1,20 @@
 <template lang="pug">
     div
-        app(:data-scheme="json")
-        app(:data-scheme="json" editor)
+        app(
+            :data-scheme="json"
+            @chooseChair="chooseChair"
+        )
+        app(
+            :data-scheme="json"
+            editor
+        )
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
     import data from "./data.json";
     import App from "@/App.vue";
+    import {Character} from "@/models/Character";
 
     @Component({
         components: {
@@ -17,6 +24,10 @@
     export default class AppWrapper extends Vue {
         get json(): string {
             return JSON.stringify(data);
+        }
+
+        chooseChair(item: Character) {
+            console.log(item);
         }
     }
 </script>
