d8e76cbf1caf2a3a3555110616890bad0629da82
Ilya Konstantinov <ilykonst95@gmail.com>
2020-05-12 19:29:13 +0400

    feat(Editor.vue)

---
+++ src/components/Editor.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    layout-cmpt(
+        :width="scheme.width"
+        :height="scheme.height"
+    )
+        character-cmpt(
+            v-for="item in scheme.characters"
+            selectable
+            :character="item"
+            :key="item.id"
+            @click="select(item)"
+        )
+
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+    import {Scheme} from "@/models/Scheme";
+    import CharacterCmpt from "@/components/Character/CharacterCmpt.vue";
+    import LayoutCmpt from "@/components/LayoutCmpt.vue";
+    import {Character} from "@/models/Character";
+    @Component({
+        components: {LayoutCmpt, CharacterCmpt}
+    })
+    export default class Editor extends Vue {
+        @Prop() readonly scheme!: Scheme;
+
+        selectedId = "";
+
+        select(item: Character): void {
+            this.selectedId = item.id;
+        }
+    }
+</script>
