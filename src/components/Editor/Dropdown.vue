c2a8aed8a8ae6052e49fbd397bb1e62abbf581cd
Ilya Konstantinov <ilykonst95@gmail.com>
2020-05-12 22:13:04 +0400

    feat(Dropdown.vue)

---
+++ src/components/Editor/Dropdown.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    .btn-group
+        slot(name="activator" :on="open")
+
+        .dropdown-menu(
+            v-show="show"
+            @mouseleave="close"
+            @click="close"
+        )
+            slot
+
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Dropdown extends Vue {
+        show = false
+
+        open() {
+            this.show = true;
+        }
+
+        close() {
+            this.show = false;
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .dropdown-menu {
+        display: block;
+    }
+</style>
